43a2a148cfd924dadc2b6bb7637d1ea6d699d7c4
lopezdaniel444 <daniellopezbbva@gmail.com>
2026-04-14 20:35:14 -0700

    Primer SELECT de alumnos con PostgreSQL y Express

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'react_express_db',
-  password: 'LOGITECH23',
-  port: 5432,
+    user: 'postgres',
+    host: 'localhost',
+    database: 'react_express_db',
+    password: 'LOGITECH23',
+    port: 5432,
 });
 
 module.exports = pool;
